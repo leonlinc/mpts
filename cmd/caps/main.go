5d2c49c918f64a1db2671442ad7fc232be98440f
leon <leon.lyy@gmail.com>
2016-12-20 13:04:00 +0800

    update

--- cmd/caps/main.go
+++ cmd/caps/main.go
@@ -2,43 +2,35 @@ package main
 
 import (
 	"encoding/binary"
+	"flag"
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/pcap"
 	"github.com/leonlinc/mpts"
 )
 
-func handlePayload(b []byte, t int64) {
-	for i := 0; i < 7; i++ {
-		offset := 188 * i
-		if b[offset] != 0x47 {
-			fmt.Println("Sync Byte Error")
-		}
-		pkt := mpts.ParseTsPkt(b[offset:])
-		if pcr, ok := pkt.PCR(); ok {
-			fmt.Println(t, pcr/27000)
-		}
-	}
-}
+var pcrFlag = flag.Bool("pcr", false, "a bool")
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Printf("Usage: %s pcap\n", os.Args[0])
-		os.Exit(0)
+	flag.Parse()
+
+	if len(flag.Args()) != 1 {
+		fmt.Printf("Usage: caps [options] <pcap-file>\n")
+		os.Exit(1)
 	}
 
-	input, output := os.Args[1], os.Args[1]+".ts"
-
+	input := flag.Args()[0]
 	handle, err := pcap.OpenOffline(input)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer handle.Close()
 
-	f, err := os.Create(output)
+	f, err := os.Create(input + ".ts")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -46,22 +38,48 @@ func main() {
 
 	source := gopacket.NewPacketSource(handle, handle.LinkType())
 	for packet := range source.Packets() {
-		appLayer := packet.ApplicationLayer()
-		if appLayer != nil {
-			payload := appLayer.Payload()
-			if len(payload) != 1316 {
-				// RTP header minimum size 12 byte
-				offset := 12
-				// Ignore CSRC for now
-				offset += 0
-				// Extension header
-				offset += 4 + 4*int(binary.BigEndian.Uint16(payload[offset+2:]))
-				payload = payload[offset:]
-			}
-			// For debugging real-time PCR jitter
-			// currTime := packet.Metadata().CaptureInfo.Timestamp.UnixNano() / 1000000
-			// handlePayload(payload, currTime)
+		payload := extract(packet)
+		if payload != nil {
 			f.Write(payload)
+		}
+	}
+}
+
+func extract(packet gopacket.Packet) (payload []byte) {
+	appLayer := packet.ApplicationLayer()
+	if appLayer != nil {
+		payload = appLayer.Payload()
+		if len(payload) != 1316 {
+			// RTP header has a minimum size of 12 bytes.
+			offset := 12
+			// TODO: assume no CSRC
+			offset += 0
+			// Extension
+			extensionLength := binary.BigEndian.Uint16(payload[offset+2:])
+			// Extension header
+			offset += 4
+			// Extension entries
+			offset += 4 * int(extensionLength)
+			payload = payload[offset:]
+		}
+		if *pcrFlag {
+			timestamp := packet.Metadata().CaptureInfo.Timestamp.UnixNano() / int64(time.Millisecond)
+			timing(payload, timestamp)
+		}
+	}
+	return
+}
+
+func timing(payload []byte, timestamp int64) {
+	for i := 0; i < 7; i++ {
+		offset := 188 * i
+		if payload[offset] != 0x47 {
+			fmt.Println("Sync Byte Error")
+		} else {
+			pkt := mpts.ParseTsPkt(payload[offset:])
+			if pcr, ok := pkt.PCR(); ok {
+				fmt.Println(timestamp, pcr/27000)
+			}
 		}
 	}
 }
